fc1a691c457b4637a195777d6d0a9348018afc60
darshan jagtap <46921438+darshanjag@users.noreply.github.com>
2023-08-11 21:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// your code here
+   document.addEventListener("DOMContentLoaded", function() {
+            const urlH3 = document.getElementById("url");
+            const form = document.getElementById("urlForm");
+            const nameInput = document.getElementById("name");
+            const yearInput = document.getElementById("year");
+            const submitButton = document.getElementById("button");
+
+            submitButton.addEventListener("click", function() {
+	            const nameValue = nameInput.value;
+                const yearValue = yearInput.value;
+                const queryString = `?name=${nameValue}?year=${yearValue}`;
+                urlH3.textContent = `https://localhost:8080/${queryString}`;
+            });
+        });
